ce5f1e3b65473423c0ff612a70fe0b0a6927d7ec
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2024-05-20 12:27:28 -0300

    Better warp card

--- src/components/WarpCard/index.tsx
+++ src/components/WarpCard/index.tsx
@@ -1,24 +1,17 @@
 import classed from "classed-components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import { faSquare } from '@fortawesome/free-solid-svg-icons/faSquare';
-import { faChartSimple } from '@fortawesome/free-solid-svg-icons/faChartSimple';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons/faDiscord';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faGitlab } from '@fortawesome/free-brands-svg-icons/faGitlab';
 import { faSteam } from '@fortawesome/free-brands-svg-icons/faSteam';
-import { faDocker } from '@fortawesome/free-brands-svg-icons/faDocker';
-import { faTerminal } from '@fortawesome/free-solid-svg-icons/faTerminal';
 import { useState } from "react";
 
 const Container = classed.div`
   container my-4 max-w-4xl p-6 mx-auto bg-linkedark sm:rounded-lg
-  flex flex-row items-center justify-center gap-2
+  flex sm:flex-row items-center justify-center gap-1 sm:gap-2
 `;
 
-const Button = classed.a`font-bold py-2 px-4 rounded-lg`;
+const Button = classed.a`font-bold py-2 px-4 rounded-lg text-center`;
 
 const LinkedInButton = classed(Button)`bg-linkeblue text-white`;
 
@@ -28,7 +21,7 @@ const GreyButton = classed(Button)`bg-linkedark-800 text-white`;
 const thatConstant = 'notjustanna.net$otliam';
 
 export default function WarpCard() {
-  const [thatHref, setThatHref] = useState('javascript:void(0)');
+  const [thatHref, setThatHref] = useState('#');
 
   const onThatHover = () => {
     const [domain,proto] = thatConstant.split('$');
@@ -49,7 +42,6 @@ export default function WarpCard() {
       <GreyButton href="https://steamcommunity.com/id/notjustanna">
         <FontAwesomeIcon icon={faSteam}/> Steam
       </GreyButton>
-      {/* that button */}
       <GreyButton href={thatHref} onMouseEnter={onThatHover}>
         <FontAwesomeIcon icon={faEnvelope}/> Email
       </GreyButton>
