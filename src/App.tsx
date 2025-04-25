8525f86e91eb18463f1a6afa7a3366820eb8ca3a
Anna <6955035+NotJustAnna@users.noreply.github.com>
2025-04-25 11:12:51 -0300

    Base rewrite done.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import {Leading} from "@/components/main-page/leading.tsx";
 import {WorkExperience} from "@/components/main-page/work-experience.tsx";
 import {KnowledgeSpotlight} from "@/components/main-page/knowledge-spotlight.tsx";
 import {ProjectSpotlight} from "@/components/main-page/project-spotlight.tsx";
+import {Footer} from "@/components/main-page/footer.tsx";
 
-function InteractiveShowcase() {
-  return <div>
-    <h2 className="text-xl font-semibold">
-      This website is more than a portfolio!
-    </h2>
-    It’s also an interactive showcase!
-  </div>;
-}
+// function InteractiveShowcase() {
+//   return <div>
+//     <h2 className="text-xl font-semibold">
+//       This website is more than a portfolio!
+//     </h2>
+//     It’s also an interactive showcase!
+//   </div>;
+// }
 
 function Main() {
   return <div className="max-[57.75rem]:mx-3 max-w-4xl min-[57.75rem]:mx-auto">
@@ -21,19 +22,8 @@ function Main() {
         <WorkExperience/>
         <KnowledgeSpotlight/>
         <ProjectSpotlight/>
-        <InteractiveShowcase/>
+        {/*<InteractiveShowcase/>*/}
       </div>
-  </div>;
-}
-
-function Footer() {
-  return <div className="bg-secondary text-secondary-foreground">
-    <div className="max-[57.75rem]:mx-3 max-w-4xl min-[57.75rem]:mx-auto py-3">
-      <h1>
-        Anna Silva
-      </h1>
-      LinkedIn ; GitHub ; Email ; Bluesky
-    </div>
   </div>;
 }
 
