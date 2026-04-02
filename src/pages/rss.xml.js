2a6e6b442fc272020ea0a2f3d3097a29947d350b
Anna Silva <git@notjustanna.net>
2026-04-02 13:45:52 -0300

    Add sorting for posts in RSS feed and new blog post on LLMs and autism

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,11 +4,16 @@ import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('posts');
+	const sortedPosts = [...posts].sort((a, b) => {
+		const aTime = new Date(a.data.pubDate ?? 0).getTime();
+		const bTime = new Date(b.data.pubDate ?? 0).getTime();
+		return bTime - aTime;
+	});
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: sortedPosts.map((post) => ({
 			...post.data,
 			link: `/post/${post.id}/`,
 		})),
